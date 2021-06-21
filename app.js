9e75c31f5e2f23959d9c4117da989342eb67999c
Mareli95 <mareli.rubio.izaguirre@gmail.com>
2021-06-20 20:36:40 -0500

    Cambiando el app.js para heroku

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const port = 3000;
+
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-	console.log('Servidor funcionando');
-});
+// app.listen(3000, () => {
+// 	console.log('Servidor funcionando');
+// });
+
+app.listen(process.env.PORT || port, () =>
+	console.log(`Servidor corriendo en puerto ${port}`)
+);
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/views/home.html');
+	res.sendFile(path.resolve('views/home.html'));
 });
 
 app.get('/login', (req, res) => {
-	res.sendFile(__dirname + '/views/login.html');
+	res.sendFile(path.resolve('views/login.html'));
 });
 
 app.get('/register', (req, res) => {
-	res.sendFile(__dirname + '/views/register.html');
-});
-
-app.get('/register', (req, res) => {
-	res.sendFile(__dirname + '/views/');
+	res.sendFile(path.resolve('views/register.html'));
 });
